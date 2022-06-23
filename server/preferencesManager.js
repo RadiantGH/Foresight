670cdc9de701554991f0617e19750d1b689a08d9
RadiantGH <iangrapeo@gmail.com>
2022-06-23 10:22:47 -0700

    Preferences for editOK and ignore added

--- server/preferencesManager.js
+++ server/preferencesManager.js
@@ -1,11 +1,13 @@
 const fs = require('fs');
 
 const preferencesManager = {};
+preferencesManager.prefPath = 'emptyprefpath';
 
 preferencesManager.projectPath = 'emptyPath';
 preferencesManager.darkMode = true;
-preferencesManager.prefPath = 'emptyprefpath';
 preferencesManager.fileExtensions = [];
+preferencesManager.editOK = [];
+preferencesManager.ignore = [];
 
 preferencesManager.getPreferences = function(preferencesPath) {
     this.prefPath = preferencesPath;
@@ -13,10 +15,18 @@ preferencesManager.getPreferences = function(preferencesPath) {
     this.projectPath = pref.projectPath;
     this.darkMode = pref.darkMode;
     this.fileExtensions = pref.fileExtensions;
+    this.editOK = pref.editOK;
+    this.ignore = pref.ignore;
 };
 
 preferencesManager.savePreferences = function() {
-    const newPref = {projectPath: this.projectPath, darkMode: this.darkMode};
+    const newPref = {
+        projectPath: this.projectPath,
+        darkMode: this.darkMode,
+        fileExtensions: this.fileExtensions,
+        editOK: this.editOK,
+        ignore: this.ignore
+    };
     fs.writeFileSync(preferencesPath, JSON.stringify(newPref, null, 2));
 };
 
